a6bc36a05d5905aa945f4f5b37e344fa10e416bd
albertrein <guilhermealberto8@gmail.com>
2021-03-15 19:44:42 -0300

    Capturando dados e tratando informações.

--- service.py
+++ service.py
@@ -2,11 +2,35 @@ from selenium import webdriver
 import time
 import json
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def volta_para_home():
 	time.sleep(4)
 	driver.find_element_by_xpath('//*[@id="sidebar-menu"]/ul/li[1]/a').click()
 
+def get_atividades():
+	atividades = []
+	itens_disciplina = driver.find_elements_by_class_name('row.p-a.m-l--22')
+	if itens_disciplina.__len__() is 0:
+		return []
+	for item in itens_disciplina:
+		if item.find_elements_by_class_name('fas.fa-star').__len__() > 0: # Possui icone da atividade
+			atividades.append(item)
+
+	return atividades
+
+
+def get_atividade_dados(atividade):
+	informacoes = atividade.find_elements_by_class_name('ng-binding') #capturando spans com os textos
+	atividade_entregue = atividade.find_elements_by_class_name('barra-amarela') #capturando spans com os textos
+	dict_atividade = ({'titulo' : informacoes[0].text, 'data' : informacoes[1].text})
+	if atividade_entregue.__len__() > 0:
+		dict_atividade.update({'status': 'concluida'})
+		return dict_atividade
+	dict_atividade.update({'status': 'nao-concluida'})
+	return dict_atividade
 
 """
 Desativa a visualização da página
@@ -14,13 +38,14 @@ options = webdriver.ChromeOptions();
 options.add_argument('headless');
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 """
-
+#captura informações de configuração
 with open('config/config.json') as file_config:
 	config = json.load(file_config)
 	file_config.close()
 
-
+#Instancia webdriver como chrome
 driver = webdriver.Chrome(ChromeDriverManager().install())
+wait = WebDriverWait(driver, 50)
 driver.get('https://servicos.ulbra.br/login/ava')
 driver.set_window_size(1024, 768)
 
@@ -36,7 +61,7 @@ proximo.click()
 
 time.sleep(3)
 
-senha = driver.find_element_by_id('senha')
+senha = wait.until(EC.presence_of_element_located((By.ID, 'senha')))
 senha.send_keys(config['senha'])
 
 
@@ -47,21 +72,30 @@ entrar.click()
 
 time.sleep(3)
 
-
+wait.until(EC.presence_of_element_located((By.TAG_NAME, 'h2')))
 lista_de_cadeiras = driver.find_elements_by_tag_name('h2')
 
+titulos_cadeiras = []
 for cadeira in lista_de_cadeiras:
-	print(cadeira.text)
+	titulos_cadeiras.append(cadeira.text)
 
 
-
+cadeiras = []
 for i in range(lista_de_cadeiras.__len__()):
+	wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'btn.btn-block.btn-success.mt-2.ng-binding'))) #cadeiras
 	links_salas_de_aula = driver.find_elements_by_class_name('btn.btn-block.btn-success.mt-2.ng-binding')
 	links_salas_de_aula[i].click()
+	try:
+		wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'row.p-a.m-l--22'))) #itens da cadeira
+	except:
+		volta_para_home()
+		continue
+	lista_atividade = get_atividades()
+	cadeiras.append({'nome': titulos_cadeiras[i], 'atividades' : []})
+	for atividade in lista_atividade:
+		cadeiras[i]['atividades'].append(get_atividade_dados(atividade))
+	
 	volta_para_home()
-	time.sleep(3)
 
 
-
-
-
+print(cadeiras)
